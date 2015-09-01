d4cca816256eba042782fa6c9cd32ba69dda6e5e
RatchyZanko <ratchapong.t@gmail.com>
2015-09-01 18:44:09 -0400

    Organized Stuff

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.get('/download/:filename', function (req, res) {
+    var file = __dirname + '/documents/' + req.params.filename;
+    res.download(file); // Set disposition and send it.
 });
 
 module.exports = router;
