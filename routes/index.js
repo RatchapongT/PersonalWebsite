f2cfa20d2b4d5ca740208e32d370b390ebb17879
RatchapongT <ratchapong.t@gmail.com>
2015-09-12 22:24:08 -0400

    priate project

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
 
+router.get('/home', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
 router.get('/download/:filename', function (req, res) {
     var file = __dirname + '/documents/' + req.params.filename;
     res.download(file); // Set disposition and send it.
@@ -18,6 +22,10 @@ router.get('/projects/firstwebsite/', function (req, res, next) {
 
 router.get('/projects/firstwebsite/mailform', function (req, res, next) {
     res.render('firstwebsite/mailform', {title: 'My First Website!'});
+});
+
+router.get('/projects/matlabship', function (req, res, next) {
+    res.render('projects/matlabship', {title: 'MATLAB Pirate Ship'});
 });
 
 router.post('/projects/firstwebsite/thankyou', function (req, res, next) {
